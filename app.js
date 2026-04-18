9710a9ec2fb5413d9774b57efb14f7962d41c3de
ZohaFatima4747 <zoha83577@gmail.com>
2026-04-18 20:43:18 +0500

    final deploy

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.use((err, req, res, _next) => {
 });
 
 // ── Start server + graceful shutdown ─────────────────────────────────────────
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 const server = app.listen(PORT, () =>
   logger.info(`Server running on port ${PORT} [${process.env.NODE_ENV}]`)
 );
